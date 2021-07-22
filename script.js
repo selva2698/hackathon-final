e5bce02cdb8dcd548cc78c10641cf1792136dcfc
selva2698 <86037526+selva2698@users.noreply.github.com>
2021-07-22 20:55:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getdata()
 {
-    const apidata = await fetch("https://makeup-api.herokuapp.com/api/v1/products.json",{
+    const apidata = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json",{
         method:"GET"
     });
     const data = await apidata.json();
@@ -10,19 +10,19 @@ function loaddata(data)
 {
     const datalist = document.createElement("div");
     datalist.className = "data-list";
-    for(let i=0; i<500; i++) {
-        const datacontainer = document.createElement("div");
-        datacontainer.className="data-container";
-        datacontainer.innerHTML=
+    data.forEach((makeup)=>{
+    const datacontainer = document.createElement("div");
+    datacontainer.className="data-container";
+    datacontainer.innerHTML=
         `
-        <img class="get-image" src=${data[i].image_link}></img>
-        <div class ="get-name">Name: ${data[i].name}</div>
-        <div class ="get-brand">Brand: ${data[i].brand}</div>
-        <div class ="get-price">Price:$${data[i].price}</div>
-        <button class ="get-plink" onclick="${data[i].product_link}">BUY NOW</button>
+        <img class="get-image"  src=${makeup.image_link}> </img>
+        <div class ="get-name">Name: ${makeup.name}</div>
+        <div class ="get-brand">Brand: ${makeup.brand}</div>
+        <div class ="get-price">Price:$${makeup.price}</div>
+        <button class ="get-plink" onclick="${makeup.product_link}">BUY NOW</button>
         `;
         datalist.append(datacontainer);
-    }
+    });
     document.body.append(datalist);
 }
 getdata();
